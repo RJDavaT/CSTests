0dd972e8814a4d82a28508a9686b48cf47720003
Ryan <Ryan.Davatwal@outlook.com>
2020-03-16 20:24:46 +0000

    Open App

---
+++ app/src/main/java/com/example/popuptest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.popuptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button bopen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bopen=findViewById(R.id.button);
+        bopen.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i=getPackageManager().getLaunchIntentForPackage("com.example.popuptest");
+                startActivity(i);
+            }
+        });
+    }
+}
